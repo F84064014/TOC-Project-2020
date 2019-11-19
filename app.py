679712ce185dc34599cafaf6b4cd121830913e50
F84064014 <aaron.chu626@gmail.com>
2019-11-19 23:37:10 +0800

    test1

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def callback():
             continue
 
         line_bot_api.reply_message(
-            event.reply_token, TextSendMessage(text="you are gay")#event.message.text)
+            event.reply_token, TextSendMessage(text=event.message.text)
         )
 
     return "OK"
@@ -113,6 +113,33 @@ def webhook_handler():
 def show_fsm():
     machine.get_graph().draw("fsm.png", prog="dot", format="png")
     return send_file("fsm.png", mimetype="image/png")
+
+
+@app.route("/test1", methods=["POST"])
+def callback():
+    signature = request.headers["X-Line-Signature"]
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # parse webhook body
+    try:
+        events = parser.parse(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    # if event is MessageEvent and message is TextMessage, then echo text
+    for event in events:
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessage):
+            continue
+
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text="hello")
+        )
+
+    return "OK"
 
 
 if __name__ == "__main__":
