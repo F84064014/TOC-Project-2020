9d200ea874261e41ed4c23417c6705aa43c98e75
F84064014 <aaron.chu626@gmail.com>
2019-11-21 12:17:25 +0800

    NULL

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import os
 import sys
-import json # test
 
 from flask import Flask, jsonify, request, abort, send_file
 from dotenv import load_dotenv
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-from twython import Twython # test
+
 
 from fsm import TocMachine
 from utils import send_text_message
