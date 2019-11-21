f73e45056397c8e89b288d5004716e47512f6939
F84064014 <aaron.chu626@gmail.com>
2019-11-21 16:59:48 +0800

    dtest

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import os
 import sys
 #===============
 import requests
-
-#import re
+#from bs4 import BeautifulSoup
+import re
 #==============
 
 
@@ -15,7 +15,7 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 from fsm import TocMachine
 from utils import send_text_message
-from bs4 import BeautifulSoup
+
 
 load_dotenv()
 
