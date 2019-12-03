a398a4c72369d774b13a07e0983e45cbeefc6458
F84064014 <aaron.chu626@gmail.com>
2019-12-03 21:06:21 +0800

    machine

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 
-resp = requests.get('https://tw.yahoo.com/')
+resp = requests.get('')
 soup = BeautifulSoup(resp.text, 'html.parser')
-stories = soup.find_all('a', class_='story-title')
-for s in stories:
-    print("title: " + s.text)
+#stories = soup.find_all('a', class_='story-title')
+#for s in stories:
+#    print("title: " + s.text)
+
+print(soup.prettify())
 
