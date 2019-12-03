cf0ff9a8efd71d4e8a7fcbf20761bb80f0069ae2
F84064014 <aaron.chu626@gmail.com>
2019-12-03 14:56:20 +0800

    test

--- app.py
+++ app.py
@@ -144,11 +144,10 @@ def test1():
         soup = BeautifulSoup(resp.text, 'html.parser')
         stories = soup.find_all('a', class_='story-title')
         #send_text_message(event.reply_token, stories.text)
-       # for s in stories:
-           # send_text_message(event.reply_token, s.text)
-        line_bot_api.reply_message(
-                event.reply_token, TextSendMessage(text = s.text)
-        )
+        for s in stories:
+            line_bot_api.reply_message(
+                    event.reply_token, TextSendMessage(text = s.text)
+            )
         line_bot_api.reply_message(
                 event.reply_token, ImageSendMessage("https://i.imgur.com/eTldj2E.png?1","https://i.imgur.com/eTldj2E.png?1")
         )
