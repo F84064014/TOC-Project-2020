ab9bb95e5cb0f3069b18e43a0bb9543c4d99a99b
F84064014 <aaron.chu626@gmail.com>
2019-12-04 23:36:06 +0800

    test

--- fsm.py
+++ fsm.py
@@ -61,8 +61,8 @@ class TocMachine(GraphMachine):
             title_url.append(s.get('href'))
         rand_title = random.randint(0, len(title))
         reply_token = event.reply_token
-        send_text_message(reply_token, title[rand_title])
-        send_text_message(reply_token, title_url[rand_title])
+        send_two_message(reply_token, title[rand_title], title_url[rand_title])
+        #send_text_message(reply_token, title_url[rand_title])
 
 
     def on_exit_state3(self):
