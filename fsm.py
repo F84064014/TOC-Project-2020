5bdcdef9a667a2ea9b3d6f18f42799864cb59217
F84064014 <aaron.chu626@gmail.com>
2019-12-04 23:09:12 +0800

    dou

--- fsm.py
+++ fsm.py
@@ -62,6 +62,7 @@ class TocMachine(GraphMachine):
         rand_title = random.randint(0, len(title))
         reply_token = event.reply_token
         send_text_message(reply_token, title[rand_title])
+        send_text_message(reply_token, title[rand_title])
 
 
     def on_exit_state3(self):
