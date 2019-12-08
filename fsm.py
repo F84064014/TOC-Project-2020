36ca391e90ab280cb4b800545d3159873c6ce787
F84064014 <aaron.chu626@gmail.com>
2019-12-08 14:05:49 +0800

    heo

--- fsm.py
+++ fsm.py
@@ -60,10 +60,10 @@ class TocMachine(GraphMachine):
         sur = list()
         for s in stitles:
             stit.append(s.text)
-        for s in surl:
-            sur.append(s.get('href'))
+        #for s in surl:
+        #    sur.append(s.get('href'))
         reply_token = event.reply_token
-        send_two_message(reply_token, stit[2], sur[2])
+        send_two_message(reply_token, stit[2], 'ok')
         self.go_back()
 
     def on_exit_state2(self):
