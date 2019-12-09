80fe6b6d95c7c21f64136b2d69f7f96b2f7b225b
F84064014 <aaron.chu626@gmail.com>
2019-12-09 20:17:24 +0800

    scrap msg

--- fsm.py
+++ fsm.py
@@ -150,6 +150,8 @@ class TocMachine(GraphMachine):
         print("I'm entering state_next")
 
         self.cur += 1
+        if self.cur >= len(self.news_url_list):
+            self.cur = 0
         self.cur_url = self.news_url_list[self.cur]
         reply_token = event.reply_token
         send_two_message(reply_token, self.news_tit_list[self.cur], self.cur_url)
