8feeebe8e850d9f4655a8faf7571b645c91158fe
F84064014 <aaron.chu626@gmail.com>
2019-12-10 20:46:21 +0800

    scrap msg

--- fsm.py
+++ fsm.py
@@ -177,7 +177,7 @@ class TocMachine(GraphMachine):
         self.news_tit_list.clear()
         self.news_url_list.clear()
         for s in surls:
-            temp.append(s.text)
+            temp.append(s.get('href'))
             self.news_url_list.append(s.get('href'))
             self.news_tit_list.append(s.get('title'))       
         x = ''.join(temp)
