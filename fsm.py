dc6f649634a05654347a2c028d1c34b91901c005
F84064014 <aaron.chu626@gmail.com>
2019-12-10 14:41:25 +0800

    scrap msg

--- fsm.py
+++ fsm.py
@@ -185,8 +185,8 @@ class TocMachine(GraphMachine):
         #    t += temp
         #    temp.append(self.news_url_list[i])
         #    t += temp
-        x = " "
-        x = x.join(self.news_tit_list)
+        #x = " "
+        x = "-".join(self.news_tit_list)
         reply_token = event.reply_token
         send_text_message(reply_token, m)
 
